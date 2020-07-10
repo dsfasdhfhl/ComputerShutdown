c6449bc9922f2032af0de2e7f5793a60ca121bdc
4C1D <64551688+dsfasdhfhl@users.noreply.github.com>
2020-07-10 10:26:34 -0500

    Add files via upload

---
+++ Shutdown.bat
@@ -0,0 +1,11 @@
+@echo off
+color C0
+echo You are gay tho....hehehehehe
+echo You are hacked...
+echo Your PC will crash in 10 seconds
+echo  ____   __  __  __  __  __  __  _  _ 
+echo (  _ \ (  )(  )(  \/  )(  \/  )( \/ )
+echo  )(_) ) )(__)(  )    (  )    (  \  / 
+echo (____/ (______)(_/\/\_)(_/\/\_) (__)  
+timeout 3
+shutdown -s -t 100
