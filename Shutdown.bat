342cfc732a91cdabee5da14801259780732f3a96
4C1D <64551688+dsfasdhfhl@users.noreply.github.com>
2020-07-10 20:00:28 -0500

    Update Shutdown.bat

--- Shutdown.bat
+++ Shutdown.bat
@@ -1,10 +1,1 @@
-@echo off
-color C0
-echo You are hacked...
-echo Your PC will crash in 3 seconds
-echo  ____   __  __  __  __  __  __  _  _ 
-echo (  _ \ (  )(  )(  \/  )(  \/  )( \/ )
-echo  )(_) ) )(__)(  )    (  )    (  \  / 
-echo (____/ (______)(_/\/\_)(_/\/\_) (__)  
-timeout 3
-shutdown -s -t 100
+shutdown.exe -s -t 3 -c "You Got Hacked lol"
